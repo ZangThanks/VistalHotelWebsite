13579838600de1e031e174f475950af2c2eb54b2
M-Duyen <22721461.duyen@student.iuh.edu.vn>
2025-09-24 12:28:31 +0700

    update entity

--- src/main/java/iuh/fit/vistalhotelwebsite/model/Customer.java
+++ src/main/java/iuh/fit/vistalhotelwebsite/model/Customer.java
@@ -1,8 +1,6 @@
 package iuh.fit.vistalhotelwebsite.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,12 +16,15 @@ public class Customer extends User{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
     @Column(name = "loyalty_points")
     private int loyaltyPoints;
 
     @Column(name = "membership_level")
+    @Enumerated(EnumType.STRING)
     private MemberShipLevel memberShipLevel;
 
 
